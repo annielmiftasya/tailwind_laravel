a50333d6573ab4b14237a4f29886f2358bff3f28
Dicky <dicky@inagata.com>
2022-02-27 09:30:53 +0700

    first init

---
+++ app/Models/Donation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'invoice', 'campaign_id', 'donatur_id', 'amount', 'pray', 'status', 'snap_token'
+    ];
+
+    public function campaign()
+    {
+        return $this->belongsTo(Campaign::class);
+    }
+
+    public function donatur()
+    {
+        return $this->belongsTo(Donatur::class);
+    }
+
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format('d-M-Y');
+    }
+
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format('d-M-Y');
+    }
+}
